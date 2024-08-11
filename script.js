55b9c5d4da3bd0cd739d66a971d79bd29bc5c805
Arulmalathi <varshnimalu@gmail.com>
2024-08-11 12:56:15 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,42 @@
+const header = document.querySelector("header");
+const hamburgerBtn = document.querySelector("#hamburger-btn");
+const closeMenuBtn = document.querySelector("#close-menu-btn");
+
+// Toggle mobile menu on hamburger button click
+hamburgerBtn.addEventListener("click", () => header.classList.toggle("show-mobile-menu"));
+
+// Close mobile menu on close button click
+closeMenuBtn.addEventListener("click", () => hamburgerBtn.click());
+
+
+
+
+$(document).ready(function () {
+    // Add smooth scrolling to all links
+    $("a").on("click", function (event) {
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $("html, body").animate(
+                {
+                    scrollTop: $(hash).offset().top,
+                },
+                800,
+                function () {
+                    // Add hash (#) to URL when done scrolling (default click behavior)
+                    window.location.hash = hash;
+                }
+            );
+        } // End if
+    })
+})
+
+
+
